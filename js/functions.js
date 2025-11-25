d65e19f3f9e6282dc6ffc2b44d5ce7e2a6a977eb
Jura91 <master2806@mail.ru>
2025-11-25 21:49:47 +0300

    The second homework assignment

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,46 @@
-const myFancyFunction  = (string = '', maxlength = 10) => {
-    return string.length - 1 <= maxlength ? true : false
+function checkLength(str, maxLength) {
+    if (str.length <= maxLength) {
+      return true;
+    }
+    return false;
+  };
 
-}
+
+  
+function isPalindrome(str) {
+    str = str.toLowerCase();
+    let cleanStr = '';
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] !== ' ') {
+        cleanStr = cleanStr + str[i];
+      }
+    }
+    let reversed = '';
+    for (let i = cleanStr.length - 1; i >= 0; i--) {
+      reversed = reversed + cleanStr[i];
+    }
+    return cleanStr === reversed;
+  };
+
+
+  
+  function extractDigits(value) {
+    value = String(value); // превращаем в строку
+  
+    let result = '';
+  
+    for (let i = 0; i < value.length; i++) {
+      const char = value[i];
+  
+      // если символ — цифра
+      if (char >= '0' && char <= '9') {
+        result = result + char;
+      }
+    }
+  
+    if (result === '') {
+      return NaN;
+    }
+  
+    return Number(result);
+  };  
