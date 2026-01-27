598882a4da8818c6d03c5f44a28b4c1810e4c1c5
Jura91 <master2806@mail.ru>
2026-01-27 19:31:50 +0300

    render-cards

---
+++ js/render-cards.js
@@ -0,0 +1,15 @@
+const template = document.querySelector('#picture').content.querySelector('.picture');
+const container = document.querySelector('.pictures');
+export const renderCards = (photos) => {
+  const fragment = document.createDocumentFragment('');
+  photos.forEach((photo) => {
+    const newCard = template.cloneNode(true);
+    const image = newCard.querySelector('.picture__img');
+    image.src = photo.url;
+    newCard.querySelector('.picture__comments').textContent = photo.comments.length;
+    newCard.querySelector('.picture__likes').textContent = photo.likes;
+    image.alt = photo.description;
+    fragment.append(newCard);
+  });
+  container.append(fragment);
+};
